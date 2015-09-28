3d279c33d86e2731630f8480c552ec8f6be7de60
aristekrat <aristekrat@gmail.com>
2015-09-27 23:51:54 -0600

    First deploy problems #5

--- app/dist/hipstertron.js
+++ app/dist/hipstertron.js
@@ -198,35 +198,35 @@ angular.module('hipstertron.controllers', [])
 angular.module('hipstertron.services', [])
 
 .factory('environmentService', ['$http',
-    function($http) {
+    function ($http) {
         return {
-            getPrefix: function() {
+            getPrefix: function () {
                 var envPrefix = {
                     prod: "http://www.hipstertron.com",
                     local: "http://localhost:9000",
                 }
-                return envPrefix['local'];
+                return envPrefix['prod'];
             },
-            getDataPrefix: function() {
+            getDataPrefix: function () {
                 var dataPrefix = {
                     dataProd: "http://hipstertron-data.herokuapp.com",
                     dataLocal: "http://localhost:8000"
                 }
-                return dataPrefix['dataLocal']
+                return dataPrefix['dataProd']
             }
         }
     }
 ])
 
 .factory('submitEmailService', ['$http', 'environmentService',
-    function($http, environmentService) {
+    function ($http, environmentService) {
         return {
-            submitEmail: function(email, callback, errorHandler) {
+            submitEmail: function (email, callback, errorHandler) {
                 $http.post(environmentService.getDataPrefix() + "/sendEmail", email)
-                    .success(function(response) {
+                    .success(function (response) {
                         return callback(response)
                     })
-                    .error(function(response) {
+                    .error(function (response) {
                         return errorHandler(response)
                     });
             }
@@ -235,13 +235,13 @@ angular.module('hipstertron.services', [])
 ])
 
 .factory('getConcertsService', ['$http', 'environmentService',
-    function($http, environmentService) {
+    function ($http, environmentService) {
         return {
-            getConcerts: function(section, callback) {
+            getConcerts: function (section, callback) {
                 $http.get(environmentService.getPrefix() + "/get-concerts/" + section, {
-                    cache: true
-                })
-                    .then(function(response) {
+                        cache: true
+                    })
+                    .then(function (response) {
                         return callback(response)
                     });
             }
